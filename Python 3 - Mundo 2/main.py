ce4246f1eb6ab9f2b3395041c84b99a2f3048367
Alexsander Melo <alexsandergmelo337@gmail.com>
2022-07-21 02:02:25 -0300

    create desafios-16.py

--- Python 3 - Mundo 2/main.py
+++ Python 3 - Mundo 2/main.py
@@ -1,1 +1,16 @@
-print("Hello, world!")
+# tuplas = ("A", "E", "I", "O", "U", "u", "o", "i", "e", "a")  # imutáveis
+# print(tuplas)
+# for i in tuplas:
+#    print(i)
+
+# for pos, i in enumerate(tuplas):
+#    print(f"Letra : {i}, posição: {pos}")
+
+# print(sorted(tuplas))
+
+
+tuplaA = (1, 5, 8)
+tuplaB = (2, 1, 10, 5)
+tuplaAB = tuplaA + tuplaB
+
+print(tuplaAB)
